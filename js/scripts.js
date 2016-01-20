45b9f4cc6a27e0334e5ed3c950b4d507f75749fc
Gracie Lundell and Nathan Otto <gracielundell@gmail.com>
2016-01-20 14:24:21 -0800

    :change tests/code to address vowels first

--- js/scripts.js
+++ js/scripts.js
@@ -2,22 +2,30 @@ var pigLatin = function(word){
   console.log(word);
   var firstLetter = word.slice(0,1);
   var secondLetter = word.slice(1,2);
+  var thirdLetter = word.slice(2,3);
+  var fourthLetter = word.slice(3,4);
   console.log(firstLetter);
   var pigWord = "";
 
-  if ((firstLetter !== "a") && (firstLetter !== "e") && (firstLetter !== "i") && (firstLetter !== "o") && (firstLetter !== "u")) {
-    pigWord = word.slice(1) + word.slice(0,1) + "ay";
-    console.log("first PW: ", pigWord);
-
-  } if ((secondLetter !== "a") && (secondLetter !== "e") && (secondLetter !== "i") && (secondLetter !== "o") && (secondLetter !== "u")) {
-    pigWord = word.slice(2) + word.slice(0,2) + "ay";
-    console.log("second PW: ", pigWord);
-
-    return false;
-
-  } else {
+  if ((firstLetter === "a") || (firstLetter === "e") || (firstLetter === "i") || (firstLetter === "o") || (firstLetter === "u")) {
     pigWord = word.concat("ay");
-    console.log("pig word:", pigWord);
-    return true;
+    console.log("first PW: ", pigWord);
+    return pigWord;
   }
+  else if ((secondLetter === "a") || (secondLetter === "e") || (secondLetter === "i") || (secondLetter === "o") || (secondLetter === "u")) {
+    pigWord = word.slice(1) + word.slice(0,1) + "ay";
+    console.log("second PW: ", pigWord);
+    return pigWord;
+  }
+  else if ((thirdLetter === "a") || (thirdLetter === "e") || (thirdLetter === "i") || (thirdLetter === "o") || (thirdLetter === "u")) {
+    pigWord = word.slice(2) + word.slice(0,2) + "ay";
+    console.log("third PW: ", pigWord);
+    return pigWord;
+  }
+  else if ((fourthLetter === "a") || (fourthLetter === "e") || (fourthLetter === "i") || (fourthLetter === "o") || (fourthLetter === "u")) {
+    pigWord = word.slice(3) + word.slice(0,3) + "ay";
+    console.log("fourth PW: ", pigWord);
+    return pigWord;
+  }
+
 };
--- specs/spec.js
+++ specs/spec.js
@@ -1,9 +1,15 @@
 describe('pigLatin', function () {
-  it("will return false if it's a consonant", function() {
-    expect (pigLatin("pie")).to.equal(false);
+  it("will add ay to the word if it begins with a vowel", function() {
+    expect (pigLatin("apple")).to.equal("appleay");
+  });
+  it("will move the first letter from the beginning to the end and add ay if the second letter is a vowel", function() {
+    expect (pigLatin("pie")).to.equal("iepay");
+  });
+  it("will move the first 2 letters from the beginning to the end and add ay if the 3rd letter is a vowel", function() {
+    expect (pigLatin("shirt")).to.equal("irtshay");
+  });
+  it("will move the first 3 letters from the beginning to the end and add ay if the 4th letter is a vowel", function() {
+    expect (pigLatin("xylophone")).to.equal("ophonexylay");
   });
 
-  it("will return false if the second letter is a consonant", function() {
-    expect (pigLatin("phish")).to.equal(false);
-  });
 });
