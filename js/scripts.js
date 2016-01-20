b6360b1d3a3f4d1d6426f5ed2788ad5847ea2200
Gracie Lundell and Nathan Otto <ottoetc@gmail.com>
2016-01-20 11:59:55 -0800

    Add second consonant check

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,23 @@
 var pigLatin = function(word){
   console.log(word);
   var firstLetter = word.slice(0,1);
+  var secondLetter = word.slice(1,2);
   console.log(firstLetter);
+  var pigWord = "";
+
   if ((firstLetter !== "a") && (firstLetter !== "e") && (firstLetter !== "i") && (firstLetter !== "o") && (firstLetter !== "u")) {
+    pigWord = word.slice(1) + word.slice(0,1) + "ay";
+    console.log("first PW: ", pigWord);
+
+  } if ((secondLetter !== "a") && (secondLetter !== "e") && (secondLetter !== "i") && (secondLetter !== "o") && (secondLetter !== "u")) {
+    pigWord = word.slice(2) + word.slice(0,2) + "ay";
+    console.log("second PW: ", pigWord);
+
     return false;
+
   } else {
-    var pigVowel = word.concat("ay");
-    console.log("pig word:", pigVowel);
+    pigWord = word.concat("ay");
+    console.log("pig word:", pigWord);
     return true;
   }
 };
--- specs/spec.js
+++ specs/spec.js
@@ -1,6 +1,9 @@
 describe('pigLatin', function () {
   it("will return false if it's a consonant", function() {
-    expect (pigLatin("apple")).to.equal(false);
+    expect (pigLatin("pie")).to.equal(false);
   });
 
+  it("will return false if the second letter is a consonant", function() {
+    expect (pigLatin("phish")).to.equal(false);
+  });
 });
